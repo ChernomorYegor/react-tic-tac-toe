31b3c96f39bfadc1ea02b48bb57c0fd1dfa66179
ChernomorYegor <chernomor.yegor@gmail.com>
2020-05-07 16:12:15 +0300

    Updates

---
+++ precache-manifest.e19544a6e1aacc9625f4ac1c7be9f649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55820988416c9f46d38b7eb7f1674fbe",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "d13e60c6858cc460241f",
+    "url": "/react-tic-tac-toe/static/css/main.02e89c45.chunk.css"
+  },
+  {
+    "revision": "f0057d47979e74efc2e7",
+    "url": "/react-tic-tac-toe/static/js/2.31832bcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.31832bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13e60c6858cc460241f",
+    "url": "/react-tic-tac-toe/static/js/main.d40f2c9d.chunk.js"
+  },
+  {
+    "revision": "4eca8d306c2d5f6566de",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.d2cea36d.js"
+  }
+]);
